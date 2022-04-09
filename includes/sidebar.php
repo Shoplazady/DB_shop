9aee3ac051f1cc55efa2fa248db4f19940104a2c
Shoplazady <103296909+Shoplazady@users.noreply.github.com>
2022-04-09 10:07:55 +0700

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,34 @@
+<div class="row">
+	<div class="box box-solid">
+	  	<div class="box-header with-border">
+	    	<h3 class="box-title"><b>Most Viewed Today</b></h3>
+	  	</div>
+	  	<div class="box-body">
+	  		<ul id="trending">
+	  		<?php
+	  			$now = date('Y-m-d');
+	  			$conn = $pdo->open();
+
+	  			$stmt = $conn->prepare("SELECT * FROM products WHERE date_view=:now ORDER BY counter DESC LIMIT 10");
+	  			$stmt->execute(['now'=>$now]);
+	  			foreach($stmt as $row){
+	  				echo "<li><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></li>";
+	  			}
+
+	  			$pdo->close();
+	  		?>
+	    	<ul>
+	  	</div>
+	</div>
+</div>
+<div class="row">
+	<div class='box box-solid'>
+	  	<div class='box-header with-border'>
+	    	<h3 class='box-title'><b>สนับสนุนเราได้ที่</b></h3>
+	  	</div>
+	  	<div class='box-body'>
+	    	<a href="https://www.facebook.com/AFB.translator/?ref=pages_you_manage" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+			<h4>แปลจนหลังเดาะV.2</h4>
+	  	</div>
+	</div>
+</div>
